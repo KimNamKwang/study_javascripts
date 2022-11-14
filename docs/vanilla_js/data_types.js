80daa21fe6467ad7d2463976b25c2369806a6c91
KimNamKwang <durante0916@gmail.com>
2022-11-14 15:14:03 +0900

    hi

---
+++ docs/vanilla_js/data_types.js
@@ -0,0 +1,12 @@
+//let declaration
+let myName = "홍길동"; // 처음 선언할때 myName에 홍길동 선언
+console.log(myName);
+myName = "박찬호"; // myName에 박찬호 선언
+myName = 5;
+console.log();
+
+//const declaration
+const nickName = "홍길동"; // 처음 선언할때 nickName에 홍길동 선언. const는 상수라 한 번 선언되면 쉽게 바뀌지 않는다.
+console.log(nickName);
+nickName = "박찬호"; // 에러. const는 값을 쉽게 바꿀 수 없음.
+console.log();
---
+++ docs/vanilla_js/input.js
@@ -0,0 +1,9 @@
+let fs = require("fs");
+
+// let input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+let readFileSync = fs.readFileSync("/dev/stdin");
+let toString = readFileSync.toString();
+let trim = toString.trim();
+let split = split.split(" ");
+
+console.log();
