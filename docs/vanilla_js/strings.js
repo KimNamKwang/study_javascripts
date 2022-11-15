7375ba90542ac5816c2de4438d997a912186440d
KimNamKwang <durante0916@gmail.com>
2022-11-15 10:31:56 +0900

    hui

---
+++ docs/vanilla_js/strings.js
@@ -0,0 +1,15 @@
+//operate number and string
+let number_first = 3;
+let string_second = "5";
+
+console.log(number_first + string_second); // +를 사용하지말고 아래처럼 `` 백틱 사용.
+let concat_string = `${number_first}${string_second}`;
+console.log(concat_string);
+
+string_second = "A";
+console.log(number_first + string_second);
+
+// Strings
+let animal = "tiger";
+console.log();
+console.log();
